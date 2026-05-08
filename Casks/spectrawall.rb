b202e8df56fbaff6347b977bcc64f2a675323f01
giggs-lynx <giggs.tsai@gmail.com>
2026-05-08 17:52:23 +0800

    feat: add spectrawall cask

--- Casks/spectrawall.rb
+++ Casks/spectrawall.rb
@@ -1,8 +1,8 @@
 cask "spectrawall" do
-  version "1.0"
+  version "0.0.1"
   sha256 "cecbef4eeb0bba966b599481ebd8c1f770998d10aec6e0255c78af477d23e34f"
 
-  url "file:///Users/giggs/lab/SpectraWall/dist/SpectraWall-#{version}.zip"
+  url "https://github.com/giggs-lynx/SpectraWall/releases/download/v#{version}/SpectraWall-#{version}.zip"
   name "SpectraWall"
   desc "Audio spectrum visualizer wallpaper"
   homepage "https://github.com/giggs-lynx/SpectraWall"
